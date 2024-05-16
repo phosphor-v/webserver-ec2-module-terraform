03ae1ee2294d80d91efe3ddcf986023b78434311
phosphor-v <volodymyrvoitovych11@gmail.com>
2024-05-16 18:53:22 +0300

    fixed circleci

--- terratest/test/main.go
+++ terratest/test/main.go
@@ -2,7 +2,6 @@ package test
 
 import (
     "testing"
-    "github.com/gruntwork-io/terratest/modules/aws"
     "github.com/gruntwork-io/terratest/modules/terraform"
     "github.com/stretchr/testify/assert"
 )
