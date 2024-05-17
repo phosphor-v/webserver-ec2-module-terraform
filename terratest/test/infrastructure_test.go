d8847cbde8259433e0c2fb2f6d10127dcbfc872a
phosphor-v <volodymyrvoitovych11@gmail.com>
2024-05-17 12:06:58 +0300

    fixed tg dir

--- terratest/test/infrastructure_test.go
+++ terratest/test/infrastructure_test.go
@@ -10,7 +10,7 @@ func TestTerraformModule(t *testing.T) {
     t.Parallel()
 
     opts := &terraform.Options{
-        TerraformDir: "../infra-module",
+        TerraformDir: "~/terratest/terragrunt/infra-module",
         TerraformBinary : "terragrunt",
     }
 
